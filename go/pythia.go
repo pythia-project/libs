eb2b6ec346d34df40aa27bbe11ba21ef6b7ad41c
Sébastien Combéfis <seb478@gmail.com>
2019-07-30 10:06:58 +0200

    Go(unit-testing): Refactor main file

--- go/pythia.go
+++ go/pythia.go
@@ -1,3 +1,22 @@
+// Pythia library for unit testing-based tasks
+// Author: Sébastien Combéfis <sebastien@combefis.be>
+//
+// Copyright (C) 2019, Computer Science and IT in Education ASBL
+// Copyright (C) 2019, ECAM Brussels Engineering School
+//
+// This program is free software: you can redistribute it and/or modify
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 of the License, or
+//  (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -6,28 +25,36 @@ import (
 	"os"
 )
 
+var fcts = map[string]func(){
+	"preprocess": preprocess,
+	"generate":   generate,
+	"feedback":   feedback,
+}
+
 func main() {
-	preprocessCmd := flag.NewFlagSet("preprocess", flag.ExitOnError)
-	generateCmd := flag.NewFlagSet("generate", flag.ExitOnError)
-	feedbackCmd := flag.NewFlagSet("feedback", flag.ExitOnError)
-
 	if len(os.Args) < 2 {
-		fmt.Println("subcommand is required")
+		fmt.Println("Subcommand is required (preprocess, generate or feedback).")
 		os.Exit(1)
 	}
+	if handle, ok := fcts[os.Args[1]]; ok {
+		handle()
+	}
+}
 
-	switch os.Args[1] {
-	case "preprocess":
-		preprocessCmd.Parse(os.Args[2:])
-		fmt.Println("preprocess")
-	case "generate":
-		generateCmd.Parse(os.Args[2:])
-		fmt.Println("generate")
-	case "feedback":
-		feedbackCmd.Parse(os.Args[2:])
-		fmt.Println("feedback")
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+func preprocess() {
+	preprocessCmd := flag.NewFlagSet("preprocess", flag.ExitOnError)
+	preprocessCmd.Parse(os.Args[2:])
+	fmt.Println("preprocess")
+}
+
+func generate() {
+	generateCmd := flag.NewFlagSet("generate", flag.ExitOnError)
+	generateCmd.Parse(os.Args[2:])
+	fmt.Println("generate")
+}
+
+func feedback() {
+	feedbackCmd := flag.NewFlagSet("feedback", flag.ExitOnError)
+	feedbackCmd.Parse(os.Args[2:])
+	fmt.Println("feedback")
 }
