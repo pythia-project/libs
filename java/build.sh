8c055a528833d0a4197e4598955d7df453926c80
Sébastien Combéfis <seb478@gmail.com>
2019-08-22 16:55:08 +0200

    Java(unit-testing): Update build script

--- java/build.sh
+++ java/build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 javac -classpath json-20180813.jar:commons-csv-1.7.jar:. -Xlint org/pythia/Execute.java
-jar cmf MANIFEST.mf pythia-1.0.jar org/pythia/*.class
+if [ $? -eq 0 ]
+then
+	jar cmf MANIFEST.mf pythia-1.0.jar org/pythia/*.class
+	if [ $? -eq 0 ]
+	then
+		echo "pythia-1.0.jar created.\nBuild succeeded!"
+		exit 0
+	fi
+fi
+echo "Build failed!"
